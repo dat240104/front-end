6a8063c70a0ef07ac15fd792b3233614f454512c
dat240104 <doitu310@gmail.com>
2025-12-19 02:05:14 +0700

    recode

--- src/App.js
+++ src/App.js
@@ -2,31 +2,28 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
- const [data, setData] = useState([]);
+ const [data, setData] = useState();
+  useEffect(() => {
+     fetch("https://backend-manguonmo.onrender.com/manguonmo")
 
-//   useEffect(() => {
-//      fetch("https://backend-tea-managerment-branch-service.onrender.com")
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((data) => {
-//         console.log("data::::", data);
-//         console.log(data);
-//         setData(data); // Lưu dữ liệu vào state
-//       })
-//       .catch((error) => console.error("Error fetching data:", error));
-//   }, []);
-// console.log(data)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data::::", data);
+        console.log(data);
+        setData(data); // Lưu dữ liệu vào state
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+console.log(data)
   return (
     <div className="App">
       <h1>thực hành phần mềm mã nguồn mở</h1>
-        fetch data from backend: Branch Service đã kết nối thành công, test sau khi coppy vao project chinh
+      <div> Dat dh42200460</div>
           <div>
-            <h2>Nguyễn Trí Cường</h2>
-            <h2>MSSV: DH52200424</h2>
-            <h2>Lớp: D22-TH08</h2>
+            <h2>data: {data}</h2>
           </div>
-            <h1>test lai</h1>
     </div>
   );
 }
